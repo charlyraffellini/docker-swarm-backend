ca25914018557bb4d5f6490c0f4115a696af05df
Carlos Esteban Raffellini <carlosraffellini@gmail.com>
2017-06-14 16:46:40 +0100

    adding sleep of 20 miliseconds

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const express = require('express');
 const rp = require("request-promise");
+const sleep = require('sleep-promise');
 
 const LISTEN_HOSTNAME = process.env.MYWEB_LISTEN_HOSTNAME || '0.0.0.0';
 const LISTEN_PORT = process.env.MYWEB_LISTEN_PORT || '3000';
@@ -8,12 +9,14 @@ const LISTEN_PORT = process.env.MYWEB_LISTEN_PORT || '3000';
 const app = express();
 
 app.get('/customer/:id', function (req, res) {
-  const customer = {
-    id: req.params.id,
-    first: "Bob",
-    last: "Doe",
-  }
-  res.json(customer);
+  sleep(20).then(function() {
+    const customer = {
+      id: req.params.id,
+      first: "Bob",
+      last: "Doe",
+    }
+    res.json(customer);
+  });
 });
 
 app.listen(LISTEN_PORT, LISTEN_HOSTNAME, function () {
